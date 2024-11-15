68f81f89b6a211af02f8c5bdce54accd625a313b
Daniel Mbengui <danielmbengui@ordinateur-portable-de-daniel.home>
2024-11-15 01:45:12 +0100

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
 
       <HomePage
         videoHeaderBack={<Stack sx={{ position: 'absolute', left: 0, right: 0, bottom: 0, top: 0, background: 'var( --blue-pudgy-shadow)' }}>
-          <video autoPlay loop muted playsInline width="640" height="360" style={{ width: "100%", height: "auto" }}>
+          <video autoPlay loop muted playsInline width="640" height="360" style={{ width: "100%", height: "100%" }}>
             <source src=
               "https://cdn.prod.website-files.com/673273ce24e1f47ec7ebf36e%2F673331c3670a32db371136c0_i_want_a_video_of_a_man_and_a_woman__walking_with_two_dogs_in_forest_617613-transcode.mp4" type="video/mp4" />
             {"Your browser does not support the video tag."}
