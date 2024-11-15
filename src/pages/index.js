acff5eb655c0d4a14c4b376429b03d7c4fc024bc
Daniel Mbengui <danielmbengui@ordinateur-portable-de-daniel.home>
2024-11-15 01:49:27 +0100

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,11 +21,28 @@ export default function Home() {
 
       <HomePage
         videoHeaderBack={<Stack sx={{ position: 'absolute', left: 0, right: 0, bottom: 0, top: 0, background: 'var( --blue-pudgy-shadow)' }}>
-          <video autoPlay loop muted playsInline width="640" height="360" style={{ width: "100%", height: "100%" }}>
-            <source src=
-              "https://cdn.prod.website-files.com/673273ce24e1f47ec7ebf36e%2F673331c3670a32db371136c0_i_want_a_video_of_a_man_and_a_woman__walking_with_two_dogs_in_forest_617613-transcode.mp4" type="video/mp4" />
-            {"Your browser does not support the video tag."}
-          </video>
+          <div style={{ position: "relative", width: "100%", height: "100vh", overflow: "hidden" }}>
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                position: "absolute",
+                top: 0,
+                left: 0
+              }}
+            >
+              <source
+                src="https://cdn.prod.website-files.com/673273ce24e1f47ec7ebf36e%2F673331c3670a32db371136c0_i_want_a_video_of_a_man_and_a_woman__walking_with_two_dogs_in_forest_617613-transcode.mp4"
+                type="video/mp4"
+              />
+              {"Your browser does not support the video tag."}
+            </video>
+          </div>
           <div style={{ position: 'absolute', left: 0, right: 0, bottom: 0, top: 0, background: 'var( --blue-pudgy-shadow)' }} />
 
         </Stack>}
